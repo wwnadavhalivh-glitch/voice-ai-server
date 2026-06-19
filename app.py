4a4ba28e1d2c9a9adaa96df259f25ece3a95e596
wwnadavhalivh-glitch <wwnadavhalivh@gmail.com>
2026-06-19 11:36:46 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,17 +2,10 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/webhook', methods=['POST'])
+@app.route('/webhook', methods=['POST', 'GET'])
 def handle_voice():
-    # פקודת הדפסה שתמיד תופיע בלוגים אם פנו אלינו
-    print("קיבלתי קריאה מ-POST!") 
-    
-    # בדיקה האם יש קבצים
-    if request.files:
-        print(f"התקבלו קבצים: {list(request.files.keys())}")
-    
-    # מחזירים "ok" מיידי כדי לא לנתק את השיחה
-    return "ok"
+    print("קיבלתי קריאה מ-POST או GET!")
+    return "OK"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
