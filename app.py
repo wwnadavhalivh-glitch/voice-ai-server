a87acf9e0802440068139d0416b43a88eb2a0831
wwnadavhalivh-glitch <wwnadavhalivh@gmail.com>
2026-07-09 15:42:38 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,27 +3,15 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    return "OK"
+
 @app.route('/webhook', methods=['GET', 'POST'])
 def webhook():
-    
-    # 1. בדיקה: האם הגענו לשלב שבו ימות המשיח סיימו להקליט ומביאים לנו את הקובץ?
-    if 'UploadFile' in request.files:
-        audio_file = request.files['UploadFile']
-        
-        # השרת לוקח את הקובץ ושומר אותו אצלו (על שרת ה-Render)
-        audio_file.save("user_question.wav")
-        print("הקובץ נשמר בהצלחה על שרת הרנדר!")
-        
-        # השרת שולח פקודה אחרונה: להגיד שהקובץ נשמר ולנתק
-        return "read=t-הקובץ נשמר בהצלחה בשרת.&hangup"
-
-
-    # 2. אם הגענו לכאן - סימן שהקובץ לא קיים, כלומר המשתמש הרגע נכנס לשלוחה!
-    # השרת מיד שולח פקודה לימות המשיח מה לעשות:
-    print("משתמש נכנס לשיחה - שולח פקודת הקלטה...")
-    
-    # הפקודה אומרת: תשמיעו לו את הטקסט, ותקליטו אותו עד שהוא יקיש סולמית
-    return "read=t-אנא הקלט את הודעתך ובסיום הקש סולמית.&api_audio_record=yes"
+    # פקודה בסיסית ביותר: רק להקריא מספר 123 ולנתק
+    # הגודל של הטקסט הזה באנגלית הוא קטן מאוד ולא יכול להתפרש לא נכון
+    return "read=num-123"
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 10000))
