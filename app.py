d77da84c3ba4dd61bfc5145329a5b214a09f5545
wwnadavhalivh-glitch <wwnadavhalivh@gmail.com>
2026-08-09 21:06:07 +0300

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def webhook():
         )
         ai_answer = response.text.strip().replace('\n', ' ')
         print(f"=== התקבלה תשובה מג'מיני: {ai_answer} ===")
-        response_text = f"id_list_message=t-{ai_answer}&read=t-כעת אנא הקלט את שאלתך הבאה לאחר הצליל ובסיום הקש סולמית=text,,voice,no,120,15,1"
+        response_text = f"read=t-{ai_answer}. כעת,האם תרצה לשאול שאלה נוספת, אז תדבר אחרי הצליל ובסיום סולמית=text,,voice&no_say_recording=yes"
         gc.collect()
         
         return Response(response_text, mimetype='text/plain; charset=utf-8')
@@ -35,7 +35,7 @@ def webhook():
     # שליחת הפקודות מופרדות בירידת שורה (\n):
     # 1. השמעת הטקסט בפורמט t-
     # 2. מעבר מידי לפקודת הקלטה וקליטת טקסט מהמשתמש
-        response_text = "read=t-שלום אנא הקלט את הודעתך לאחר הצליל ובסיום הקש סולמית=text,,voice,no,120,15,1"
+        response_text = "read=t-שלום, אנא הקלט את הודעתך לאחר הצליל ובסיום הקש סולמית=text,,voice,no_say_recording=yes"
         gc.collect()
         return Response(response_text, mimetype='text/plain; charset=utf-8')
 
